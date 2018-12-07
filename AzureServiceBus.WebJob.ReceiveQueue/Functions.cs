a15832d15b94670ad7ae9db9770df1418279f345
Faruk Aydemir <faydemir@outlook.com>
2018-12-07 15:06:34 +0300

    first commir

---
+++ AzureServiceBus.WebJob.ReceiveQueue/Functions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.ServiceBus.Messaging;
+
+namespace AzureServiceBus.WebJob.ReceiveQueue
+{
+    public class Functions
+    {
+        // This function will get triggered/executed when a new message is written 
+        // on an Azure Queue called queue.
+        public static async Task ProcessQueueMessage([ServiceBusTrigger("queue")]  BrokeredMessage message, TextWriter log)
+        {
+            Stream stream = message.GetBody<Stream>();
+            StreamReader reader = new StreamReader(stream);
+            string queueMessage = reader.ReadToEnd();
+
+            await PostWebApiAsync(queueMessage);
+            //Log
+            log.WriteLine("Message Body : " + queueMessage);
+        }
+
+
+        public static async Task<string> PostWebApiAsync(string postData)
+        {
+            var client = new HttpClient {Timeout = TimeSpan.FromMinutes(2)};
+            var result = await client.GetAsync($"url");
+            string resultContent = await result.Content.ReadAsStringAsync();
+            client.Dispose();
+            return resultContent;
+
+        }
+    }
+}
---
+++ AzureServiceBus.WebJob.ReceiveQueue/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AzureServiceBus.WebJob.ReceiveQueue")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AzureServiceBus.WebJob.ReceiveQueue")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1b6c770a-d998-44ea-b619-d2a2d41b34a5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
